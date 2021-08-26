dfa902c829f542730f5f9e4d586a0a113acbcd96
kimsunwoo <sgk0424@naver.com>
2021-08-26 21:02:26 +0900

    선우 수정

--- src/mainClass/MainClass.java
+++ src/mainClass/MainClass.java
@@ -2,12 +2,11 @@ package mainClass;
 
 import java.util.Scanner;
 
-<<<<<<< HEAD
-=======
+
 import five.Five;
 import raina.RainaCase;
 import soomin.soomin_func;
->>>>>>> bf9333c95d13e0054ded9ff34cbd1d92f781ae37
+import sunwoo.Test;
 
 public class MainClass {
 
@@ -15,28 +14,21 @@ public class MainClass {
 		Five five = new Five();
 		RainaCase raina = new RainaCase();
 		soomin_func soomin = new soomin_func();
-		
 		Scanner sc = new Scanner(System.in);
 		sunwoo.Test tes=new sunwoo.Test();
 		System.out.println("1.강동원 2.김선우 3.박수민 4.김나현 5.김도윤");
 		int num = sc.nextInt();
 		
 		switch(num) {
-		case 1: break;
-		case 2: break;
-<<<<<<< HEAD
-		case 3: break;
-		case 4: 
+		case 1: 
+			break;
+		case 2: 
 			tes.dis();
 			break;
-		case 5: break;
-=======
 		case 3: soomin.one(); break;
 		case 4: raina.hi(); break;
 		case 5: five.selectFive();
 				break;
-		
->>>>>>> bf9333c95d13e0054ded9ff34cbd1d92f781ae37
 		
 		}
 		
