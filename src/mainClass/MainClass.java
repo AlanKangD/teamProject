9ad46eec2706e70dad0d67fbac128174ceccb455
AlanKangD <kno12350@gmail.com>
2021-08-26 17:41:18 +0900

    첫번째alan test 중입니다.

--- src/mainClass/MainClass.java
+++ src/mainClass/MainClass.java
@@ -2,14 +2,17 @@ package mainClass;
 
 import java.util.Scanner;
 
+import alanFile.AlanFlie;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("1.강동원 2.김선우 3.박수민 4.김나현 5.김도윤");
 		int num = sc.nextInt();
+		AlanFlie  alan = new AlanFlie();
 		switch(num) {
-		case 1: break;
+		case 1: alan.alanTest(); break;
 		case 2: break;
 		case 3: break;
 		case 4: break;
