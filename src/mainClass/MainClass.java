e59ed40655a8990f6221fe84788f30f62f214604
niyaoh <doyoonkim980818@gmail.com>
2021-08-26 17:38:37 +0900

    case 5 기능 구현

--- src/mainClass/MainClass.java
+++ src/mainClass/MainClass.java
@@ -2,9 +2,12 @@ package mainClass;
 
 import java.util.Scanner;
 
+import five.Five;
+
 public class MainClass {
 
 	public static void main(String[] args) {
+		Five five = new Five();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("1.강동원 2.김선우 3.박수민 4.김나현 5.김도윤");
 		int num = sc.nextInt();
@@ -13,7 +16,8 @@ public class MainClass {
 		case 2: break;
 		case 3: break;
 		case 4: break;
-		case 5: break;
+		case 5: five.selectFive();
+				break;
 		
 		}
 		
