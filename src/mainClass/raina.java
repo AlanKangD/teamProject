a5719f7e936aeb4a95f03bf85a1685a5ff46d1f7
raina <skgus5598@gmail.com>
2021-08-26 21:35:04 +0900

    다시 올려봐

--- src/mainClass/raina.java
+++ src/mainClass/raina.java
@@ -4,6 +4,9 @@ public class raina {
 
 	public void hi() {
 		System.out.println("hello");
+		System.out.println("개인 브랜치로가서 파일을 수정하고 ");
+		System.out.println("다시 해 봅시다");
+		System.out.println("bye");
 		
 	}
 }
