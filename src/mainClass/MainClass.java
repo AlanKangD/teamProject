76e05e38277c03eb499e45f85124439af89bcb16
kimsunwoo <sgk0424@naver.com>
2021-08-26 20:16:10 +0900

    선우수정

--- src/mainClass/MainClass.java
+++ src/mainClass/MainClass.java
@@ -2,17 +2,21 @@ package mainClass;
 
 import java.util.Scanner;
 
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		sunwoo.Test tes=new sunwoo.Test();
 		System.out.println("1.강동원 2.김선우 3.박수민 4.김나현 5.김도윤");
 		int num = sc.nextInt();
 		switch(num) {
 		case 1: break;
 		case 2: break;
 		case 3: break;
-		case 4: break;
+		case 4: 
+			tes.dis();
+			break;
 		case 5: break;
 		
 		}
