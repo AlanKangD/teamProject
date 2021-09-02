dd69687e2fe3afe00350df7464b6ea991e232996
AlanKangD <kno12350@gmail.com>
2021-09-02 21:41:54 +0900

    기본 틀

--- src/Memeber/MainClass.java
+++ src/Memeber/MainClass.java
@@ -11,11 +11,11 @@ public class MainClass {
 			System.out.println("1. 회원가입 2. 회원전체조회 3. 회원정보검색 4. 회원수정 5. 회원삭제 6. 댓글관리");
 			num = sc.nextInt();
 			switch(num) {
-			case 1: break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
-			case 5: break;
+			case 1: break; //도윤
+			case 2: break; //나현
+			case 3: break; //수민
+			case 4: break; //선우
+			case 5: break; //동수
 			case 6: break; //댓글관리에서는 기능으로 해당 회원이 쓴 댓글만 볼수 있게 만들어 줍니다. 
 			
 			}
