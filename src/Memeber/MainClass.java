42d16c919dfbecf91d2b490a86793cbde0a8ac0f
kimsunwoo <sgk0424@naver.com>
2021-09-03 18:03:49 +0900

    회원수정기능구현

--- src/Memeber/MainClass.java
+++ src/Memeber/MainClass.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num;
-		
+		int num=0;
+		MembersunwooDB db=new MembersunwooDB();
+		MembersunwooDAO dao=new MembersunwooDAO();
 		while(true) {
 			System.out.println("1. 회원가입 2. 회원전체조회 3. 회원정보검색 4. 회원수정 5. 회원삭제 6. 댓글관리");
 			num = sc.nextInt();
@@ -14,7 +15,9 @@ public class MainClass {
 			case 1: break; //도윤
 			case 2: break; //나현
 			case 3: break; //수민
-			case 4: break; //선우
+			case 4: 
+				dao.display();
+				break; //선우
 			case 5: break; //동수
 			case 6: break; //댓글관리에서는 기능으로 해당 회원이 쓴 댓글만 볼수 있게 만들어 줍니다. 
 			
---
+++ src/Memeber/MembersunwooDB.java
@@ -0,0 +1,40 @@
+package Memeber;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class MembersunwooDB {
+	private String url = "jdbc:oracle:thin:@localhost:xe";
+	private String id = "java";
+	private String pwd = "1234";
+	public void DBClass() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+public int updateData(String id,String name,int password,String addr,String reply) {
+	int result=0;
+	Connection con=null;
+	PreparedStatement ps=null;
+	String sql="update teammember set name=?,password=?,addr=?,reply=? where id=?";
+	try {
+		con=DriverManager.getConnection(url,id,pwd);
+		ps=con.prepareStatement(sql);
+		ps.setString(5, id);
+		ps.setString(4, reply);
+		ps.setString(3,addr);
+		ps.setInt(2, password);
+		ps.setString(1, name);
+		result = ps.executeUpdate();
+	}catch(Exception e){
+		e.printStackTrace();
+	}
+	return result;
+}
+}
