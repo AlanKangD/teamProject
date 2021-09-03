c7e76e7d821f2ed3a488031881fa721f7b7b90a4
jindongsu <dsjin123@naver.com>
2021-09-03 18:10:58 +0900

    최종 삭제기능 동수

--- src/Memeber/MemberJinDB.java
+++ src/Memeber/MemberJinDB.java
@@ -7,11 +7,16 @@ public class MemberJinDB {
 	String id = "foracle";
 	String pw = "1234";
 	
+	public MemberJinDB() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+	
 	public int deleteMem(String userId) {
 		int result = 0;
 		String sql = "delete from teamMember where id = '"+userId+"'";
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection con = DriverManager.getConnection(url, id, pw);
 			PreparedStatement ps = con.prepareStatement(sql);
 			result = ps.executeUpdate();
